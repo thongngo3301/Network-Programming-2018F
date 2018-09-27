afd9d697cae9cd946c3529206163db44d1026d8d
thongngo3301 <thongngo3301@gmail.com>
2018-09-27 22:38:13 +0700

    add BTH2

---
+++ BTH2/Client/client.c
@@ -0,0 +1,86 @@
+/**
+ * Name: Ngo Doan Thong
+ * MSSV: 16022451
+ * Description:
+ *    - User type server's IP address and port from keyboard
+ *    - User type message from keyboard
+ *    - Client read and send message to Server
+ *    - Client display response from Server
+ *    - User type 'end' to close connection
+**/
+
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main() {
+	const int _family = AF_INET;	// IPv4
+	const int _type = SOCK_STREAM;	// TCP
+	const int _protocol = 0;
+	const int _bufferLength = 1024;
+	const char *_terminateChar = "QUIT";
+
+	int serverSocket;
+	struct sockaddr_in serverAddr;
+
+	char* serverIpAddr = (char *) malloc(100 * sizeof(char *));
+	int serverPort;
+
+	serverSocket = socket(_family, _type, _protocol);
+	if (serverSocket < 0) {
+		perror("Server socket error");
+		return 1;
+	}
+
+	bzero(&serverAddr, sizeof(serverAddr));
+
+	printf("Server IP Address: ");
+	scanf("%s", serverIpAddr);
+	printf("Server Port: ");
+	scanf("%d", &serverPort);
+	getchar();
+
+	serverAddr.sin_addr.s_addr = inet_addr(serverIpAddr);
+	serverAddr.sin_port = htons(serverPort);
+	serverAddr.sin_family = _family;
+
+	setsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, _bufferLength, sizeof(bufferLength));
+
+	// Connect to server by using serverSocket
+	int connCheck = connect(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	if (connCheck < 0) {
+		perror("Connect error");
+		return 1;
+	}
+
+	printf("Connected to server. Send 'QUIT' to stop.\n");
+	printf("Enter file name to download:");
+
+	// Send file name to server and receive file from server
+	char sendBuffer[_bufferLength];
+	char receiveBuffer[_bufferLength];
+
+	while (1) {
+		fgets(sendBuffer, sizeof(sendBuffer), stdin);
+		sendBuffer[strlen(sendBuffer) - 1] = '\0';
+
+		if (strcmp(sendBuffer, _terminateChar) == 0) {
+			break;
+		}
+
+		int sentMsgLen = strlen(sendBuffer) + 1;		
+		write(serverSocket, sendBuffer, sentMsgLen);
+
+		read(serverSocket, receiveBuffer, sizeof(receiveBuffer));
+		printf("Response from server: %s\n", receiveBuffer);
+	}
+
+	close(serverSocket);
+	printf("Closed connection.\n");
+
+	return 0;
+}
---
+++ BTH2/Server/server.c
@@ -0,0 +1,103 @@
+/**
+ * Name: Ngo Doan Thong
+ * MSSV: 16022451
+ * Description:
+ *    - Server open port to listen to connection from Client
+ *    - Server receive message from Client
+ *    - Server change all characters in message into upper case
+ *    - Server send modified message back to Client
+ *    - When receive terminate character, Server close connection
+**/
+
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdio.h>
+
+int main() {
+	const int _family = AF_INET;	// IPv4
+	const int _type = SOCK_STREAM;	// TCP
+	const int _protocol = 0;
+	const int _port = 9090;
+	const int _listenQueue = 1024;	// Backlog in listen()
+	const int _bufferLength = 1024;
+	const char *_terminateChar = "QUIT";
+
+	int serverSocket, connClientSocket;
+	struct sockaddr_in serverAddr, connClientAddr;
+
+	// Create a socket to listen
+	serverSocket = socket(_family, _type, _protocol);
+	if (serverSocket < 0) {
+		perror("Server socket error");
+		return 1;
+	}
+
+	bzero(&serverAddr, sizeof(serverAddr));
+
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(_port);
+	serverAddr.sin_family = _family;
+
+	int bindCheck = bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	if (bindCheck < 0) {
+		perror("Bind error");
+		return 1;
+	}
+
+	int listenCheck = listen(serverSocket, _listenQueue);
+	if (listenCheck < 0) {
+		perror("Listen error");
+		return 1;
+	}
+
+	printf("Server is listening at port %d\n", _port);
+
+	unsigned int addrLength = sizeof(connClientAddr);
+
+	while ((connClientSocket = accept(serverSocket, (struct sockaddr *) &connClientAddr, &addrLength)) >= 0) {
+		// Get client info
+		char *clientIpAddr = inet_ntoa(connClientAddr.sin_addr);
+		int clientPort = ntohs(connClientAddr.sin_port);
+
+		printf("Client address: %s:%d\n", clientIpAddr, clientPort);
+
+		// Receive file name from client and send file back
+		char buffer[_bufferLength];
+		char fileName[_bufferLength];
+		// Read the requested file name
+		while (read(connClientSocket, fileName, sizeof(fileName)) > 0) {
+			if (strcmp(fileName, _terminateChar) == 0) {
+				break;
+			}
+			printf("File '%s' is being sent...\n", fileName);
+
+			// Get the requested file
+			FILE *file = fopen(fileName, "rb");
+			// Send file length first
+			fseek(file, 0, SEEK_END);
+			int size = ftell(file);
+			fseek(file, 0, SEEK_SET);
+			printf("File size: %d\n", size);
+			write(connClientSocket, (void *)&size, sizeof(int));
+			// Send file
+			while (!feof(file)) {
+				// Read file to buffer
+				int readSize = fread(buffer, 1, sizeof(buffer) - 1, file);
+				write(connClientSocket, buffer, readSize);
+				// Zero out buffer after writing
+				bzero(buffer, sizeof(buffer));
+			}
+			printf("Sent file successfully!\n");
+		}
+		printf("Session ended");
+		close(connClientSocket);
+	}
+	close(serverSocket);
+	printf("Closed connection.\n");
+
+	return 0;
+}
